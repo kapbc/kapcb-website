a1891dc814e1efcece3026a9fa716cd53015bc6a
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-01 19:23:18 +0800

    add config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+var a = '!11'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// 对外暴露一个对象
+module.exports = {
+  // 关闭eslint
+  lintOnSave: false
+
+}
