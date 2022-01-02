16f2e99988286cfede8f41dbd583dad63888c555
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-02 22:24:23 +0800

    re write Vue Router's push method

--- src/pages/router/index.js
+++ src/pages/router/index.js
@@ -11,6 +11,32 @@ const login = () => import('@/pages/login/index')
 const register = () => import('@/pages/register/index')
 const search = () => import('@/pages/search/index')
 
+// 重写VueRouter原型对象的push方法解决编程式路由跳转连续点击promise错误回调
+// 先将VueRouter原型对象上的push方法进行备份, 因为最终使用的还是原型对象上的push方法
+let originalVueRouterPush = VueRouter.prototype.push
+
+// 重写push|replace方法
+// 第一个参数 : 告诉原来push方法, 需要跳转的location
+VueRouter.prototype.push = function (location, resolve, reject) {
+
+  // 如果传递了成功回调函数与失败回调函数
+  if (resolve && reject) {
+    // call || apply 的区别 :
+    // 相同点 : 都可以调用函数一次, 都可以篡改函数的上下文一次
+    // 不同点 : call与apply传递参数的方式不同, call传递多个参数使用','隔开, 而apply传递多个参数使用数组
+    // this为当前调用push方法的vue组件实例
+    originalVueRouterPush.call(this, location, resolve, reject)
+  } else {
+    originalVueRouterPush.call(this, location,
+      () => {
+      },
+      () => {
+      })
+  }
+
+}
+
+
 // 配置路由
 // 全局暴露 VueRouter 组件
 export default new VueRouter({
