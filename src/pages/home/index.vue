4ab24661987baad625f015b5a00f9305333950f3
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-03 17:13:22 +0800

    add list container

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,13 +3,19 @@
     我是首页
     <!-- 三级联动全局组件 categoryNav, 已经在main.js中进行全局注册, 不需要在使用import导入 -->
     <categoryNav></categoryNav>
+
+    <container></container>
   </div>
 </template>
 
 <script>
+const container = () => import('@/pages/home/container/index')
 
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    container
+  }
 }
 
 </script>
