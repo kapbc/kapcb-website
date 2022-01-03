664fd9beef9c4d1fcb71017b2deb17d691f0f67e
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-03 17:02:14 +0800

    add category nav component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 // 引入路由
 import router from '@/pages/router/index'
 
+// 注册全局组件
+const categoryNav = () => import('@/pages/home/nav/index')
+
+// 第一个参数 : 全局组件的名称
+// 第二个参数 : 组件
+Vue.component(categoryNav.name, categoryNav)
+
+
 Vue.config.productionTip = false
 
 new Vue({
@@ -11,5 +19,5 @@ new Vue({
   // 当在这里注册 router 之后, 所有的组件(路由组件&非路由组件)中都拥有@route & $router属性
   // $route(route -> 路由) : 一般是获取路由信息, 如获取路径、params、path等等
   // $router(router -> 路由器) : 一般是进行编程式导航, 如进行路由跳转如push | replace
-  router
+  router,
 }).$mount('#app')
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     我是首页
+    <!-- 三级联动全局组件 categoryNav, 已经在main.js中进行全局注册, 不需要在使用import导入 -->
+    <categoryNav></categoryNav>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'index'
+  name: 'home'
 }
 
 </script>
